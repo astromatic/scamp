55aeed1f9a76395f3965d4f987c81018c2e56946
Teake Nutma <t.a.nutma@rug.nl>
2019-06-18 16:34:20 +0200

    Fix buffer overflow when printing long filenames

--- src/match.c
+++ src/match.c
@@ -106,7 +106,7 @@ static void putgauss(float *histo, int width,int height, double x,double y,
 void match_field(fieldstruct *field, fieldstruct *reffield)
 {
     setstruct *refset, *refset2, *set, *fieldset;
-    char  str[128];
+    char  str[MAXCHAR];
     double wcspos[NAXIS],
     angle,scale,dlng,dlat,sig, dlng2,dlat2,sig2, asig,
     dangle,dscale, ddlng,ddlat, shear,sangle, sheartot, sangletot,
@@ -114,7 +114,7 @@ void match_field(fieldstruct *field, fieldstruct *reffield)
     int  i,j,k, lng,lat,
          naxis, nmax;
 
-    sprintf(str, "Matching field %s...", field->rfilename);
+    sprintf(str, "Matching field %.*s ...", MAXCHAR - 20, field->rfilename);
     NFPRINTF(OUTPUT, str);
 
     /* We are going to use FFT routines */
