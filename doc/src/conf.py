259664f086b0c2d1f89c59975febaa0a17780670
Emmanuel Bertin <bertin@iap.fr>
2021-02-10 22:20:53 +0100

    Fixed regression in command line parsing that would prevent negative numbers in the first arguments.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -81,7 +81,7 @@ language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
-today = 'Thu Dec 03 2020'
+today = 'Wed Feb 10 2021'
 #
 # Else, today_fmt is used as the format for a strftime call.
 #
